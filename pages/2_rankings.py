8add67d504a6ba0fc40854b00c233cc6a5db0a97
Bernardo Pereira <belopespereira@gmail.com>
2026-01-28 01:23:49 -0300

    fix(rankings): force deployment to resolve NameError

--- pages/2_rankings.py
+++ pages/2_rankings.py
@@ -91,7 +91,10 @@ OUTCOMES = ["Sucesso", "Falha"]
 QUALIFIERS = ["KeyPass", "Assisted", "BigChanceCreated", "LeadingToGoal", "LeadingToAttempt", "Head", "Cross", "Corner", "FreeKick", "Penalty", "Throughball", "Longball", "Chipped", "LayOff", "Volley", "OwnGoal", "Red", "Yellow"]
 
 
+
+# Defining layout columns for filters (Force Update)
 col_c1, col_c2, col_c3, col_c4 = st.columns([1.5, 1.5, 2, 0.5])
+
 
 with col_c1:
 
